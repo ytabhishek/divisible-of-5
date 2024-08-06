a041fad46529dba68fb5f12a4999841c0f757854
Abhishek Pratap Singh <ytabhishek721@gmail.com>
2024-08-06 23:54:23 +0530

    Add files via upload

---
+++ diisible.java
@@ -0,0 +1,17 @@
+package conditional;
+import java.util.Scanner;
+public class diisible {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        if(n%5==0){
+            System.out.println("The number is divisible by 5 ");
+
+        }
+        else{
+            System.out.println("The number is not divisible by 5");
+        }
+
+    }
+}
